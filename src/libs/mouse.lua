e599ff51cd8d4462fb301bba97e973f6e1d8a411
1e20 <98073814+1e20@users.noreply.github.com>
2022-11-16 19:42:45 -0800

    Create mouse.lua

---
+++ src/libs/mouse.lua
@@ -0,0 +1,47 @@
+local UserInputService = game:GetService('UserInputService');
+local Vector2 = Vector2.new;
+local Sleep = task.wait;
+
+local Mouse = {};
+Mouse.Position = UserInputService:GetMouseLocation();
+
+function Mouse:Move(Position, Smoothness) Smoothness = Smoothness or 1
+    mousemoverel((Position.X - self.Position.X) / Smoothness, (Position.Y - self.Position.Y) / Smoothness);
+end;
+
+-- // Spring
+Mouse.Spring = {
+    TargetPosition = Vector2(0, 0);
+    Position = Vector2(0, 0);
+    VelocityX = 0;
+    VelocityY = 0;
+    Tension = 0.05;
+    Dampening = 0.1; 
+}
+
+function Mouse.Spring:Update()
+    local Difference = (self.TargetPosition - self.Position)
+    self.VelocityX = (self.VelocityX + self.Tension * Difference.X - self.VelocityX * self.Dampening)
+    self.VelocityY = (self.VelocityY + self.Tension * Difference.Y - self.VelocityY * self.Dampening)
+    self.Position = Vector2(self.Position.X + self.VelocityX, self.Position.Y + self.VelocityY)
+end; 
+
+function Mouse.Spring:Move(Position, Smoothness) Smoothness = Smoothness or 1
+    self.TargetPosition = Position;
+    self.Position = Mouse.Position;
+    self:Update();
+
+    mousemoverel(
+        (self.Position.X - Mouse.Position.X) / Smoothness,
+        (self.Position.Y - Mouse.Position.Y) / Smoothness
+    );
+end; 
+
+-- Listeners
+Mouse.ChangedListener = UserInputService.InputChanged:Connect(function(Input)
+    if (Input.UserInputType == Enum.UserInputType.MouseMovement) then 
+        Mouse.Position = UserInputService:GetMouseLocation();
+    end; 
+end);
+
+return Mouse;
