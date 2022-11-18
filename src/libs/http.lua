8deba52604c0162561464449b1dbc245bb29258f
1e20 <98073814+1e20@users.noreply.github.com>
2022-11-18 15:48:32 -0800

    Update http.lua

--- src/libs/http.lua
+++ src/libs/http.lua
@@ -1,15 +1,17 @@
-local Http = {};
+local Http, __Http = {}, newproxy(true);
+getmetatable(__Http).__index = Http;
 Http.Directory = '1e20/Cybin/main/';
-Http.__Host = 'https://raw.githubusercontent.com/';
+Http.Host = 'https://raw.githubusercontent.com/';
 
 function Http:Import(PaTh, As)
-    local Data = loadstring(game:HttpGet(self.__Host .. self.Directory .. Directory))();
+    local Data = loadstring(game:HttpGet(self.Host .. self.Directory .. Directory))();
     if (As) then getgenv()[As] = Data; end;
     return Data;
 end;
 
-function Http:SetDirectory(Directory)
+function Http:SetData(Host, Directory)
     self.Directory = Directory;
+    self.Host = Host;
 end; 
 
-return Http;
+return __Http;
