81ddba8a42d8250639f67dae4b48b332cf044a0d
1e20 <98073814+1e20@users.noreply.github.com>
2022-11-19 09:50:35 -0800

    Update http.lua

--- src/libs/http.lua
+++ src/libs/http.lua
@@ -1,6 +1,7 @@
 local Http, __Http = {}, newproxy(true);
 getmetatable(__Http).__index = Http;
 getmetatable(__Http).__newindex = Http;
+getmetatable(__Http).__namecall = Http;
 Http.Directory = '1e20/Cybin/main/';
 Http.Host = 'https://raw.githubusercontent.com/';
 
